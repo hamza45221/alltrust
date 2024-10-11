8adae510750dfb897d23b27365c9507d05ebc8f8
Hamza <hj1087546@gmail.com>
2024-10-10 23:13:24 -0700

    ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,234 @@
-@extends('layouts.app')
+<!doctype html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<html
+    lang="en"
+    class="light-style layout-wide customizer-hide"
+    dir="ltr"
+    data-theme="theme-default"
+    data-assets-path="public/assets/"
+    data-template="vertical-menu-template"
+    data-style="light">
+<head>
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <title>Login Cover - Pages | Vuexy - Bootstrap Admin Template</title>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <meta name="description" content="" />
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="public/assets/img/favicon/favicon.ico" />
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="public/assets/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="public/assets/vendor/fonts/tabler-icons.css" />
+    <link rel="stylesheet" href="public/assets/vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+
+    <link rel="stylesheet" href="public/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="public/assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+
+    <link rel="stylesheet" href="public/assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="public/assets/vendor/libs/node-waves/node-waves.css" />
+
+    <link rel="stylesheet" href="public/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="public/assets/vendor/libs/typeahead-js/typeahead.css" />
+    <!-- Vendor -->
+    <link rel="stylesheet" href="public/assets/vendor/libs/@form-validation/form-validation.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="public/assets/vendor/css/pages/page-auth.css" />
+
+    <!-- Helpers -->
+    <script src="public/assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="public/assets/vendor/js/template-customizer.js"></script>
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="public/assets/js/config.js"></script>
+
+    <style>
+
+        .app-brand-logo.demo {
+            -ms-flex-align: center;
+            align-items: center;
+            -ms-flex-pack: center;
+            justify-content: center;
+            display: -ms-flexbox;
+            display: flex;
+            width: 200px;
+            height: 68px;
+        }
+    </style>
+
+</head>
+
+<body>
+<!-- Content -->
+
+<div class="authentication-wrapper authentication-cover">
+    <!-- Logo -->
+    <a href="index.html" class="app-brand auth-cover-brand">
+        <span class="app-brand-logo demo">
+           <img src="{{url('public/assets/Logo.png')}}" alt="Taxi-Link Logo" width="100%" height="115px"/>
+        </span>
+{{--        <span class="app-brand-text demo text-heading fw-bold">Vuexy</span>--}}
+    </a>
+    <!-- /Logo -->
+    <div class="authentication-inner row m-0">
+        <!-- /Left Text -->
+        <div class="d-none d-lg-flex col-lg-8 p-0">
+            <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+                <img
+                    src="../../assets/img/illustrations/auth-login-illustration-light.png"
+                    alt="auth-login-cover"
+                    class="my-5 auth-illustration"
+                    data-app-light-img="illustrations/auth-login-illustration-light.png"
+                    data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
+
+                <img
+                    src="../../assets/img/illustrations/bg-shape-image-light.png"
+                    alt="auth-login-cover"
+                    class="platform-bg"
+                    data-app-light-img="illustrations/bg-shape-image-light.png"
+                    data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+            </div>
+        </div>
+        <!-- /Left Text -->
+
+        <!-- Login -->
+        <div class="d-flex col-12 col-lg-4 align-items-center authentication-bg p-sm-12 p-6">
+            <div class="w-px-400 mx-auto mt-12 pt-5">
+                <h4 class="mb-1">Welcome to Vuexy! 👋</h4>
+                <p class="mb-6">Please sign-in to your account and start the adventure</p>
+
+                <form id="formAuthentication" class="mb-6" action="{{ route('login') }}" method="Post">
+                    @csrf
+                    <div class="mb-6">
+                        <label for="email" class="form-label">Email</label>
+                        <input
+                            type="text"
+                            class="form-control @error('email') is-invalid @enderror"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            autofocus />
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                        @enderror
+                    </div>
+                    <div class="mb-6 form-password-toggle">
+                        <label class="form-label" for="password">Password</label>
+                        <div class="input-group input-group-merge">
+                            <input
+                                type="password"
+                                id="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                name="password"
+                                placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                aria-describedby="password" />
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                            <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                    </div>
+                    <div class="my-8">
+                        <div class="d-flex justify-content-between">
+                            <div class="form-check mb-0 ms-2">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember-me" {{ old('remember') ? 'checked' : '' }}/>
+                                <label class="form-check-label" for="remember-me"> Remember Me </label>
                             </div>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}">
+                                    <p class="mb-0">Forgot Password?</p>
+                                </a>
+                            @endif
                         </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary d-grid w-100">Sign in</button>
+                </form>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                <p class="text-center">
+                    <span>New on our platform?</span>
+                    <a href="{{ route('register') }}">
+                        <span>Create an account</span>
+                    </a>
+                </p>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="divider my-6">
+                    <div class="divider-text">or</div>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <a href="javascript:;" class="btn btn-sm btn-icon rounded-pill btn-text-facebook me-1_5">
+                        <i class="tf-icons ti ti-brand-facebook-filled"></i>
+                    </a>
+
+                    <a href="javascript:;" class="btn btn-sm btn-icon rounded-pill btn-text-twitter me-1_5">
+                        <i class="tf-icons ti ti-brand-twitter-filled"></i>
+                    </a>
+
+                    <a href="javascript:;" class="btn btn-sm btn-icon rounded-pill btn-text-github me-1_5">
+                        <i class="tf-icons ti ti-brand-github-filled"></i>
+                    </a>
+
+                    <a href="javascript:;" class="btn btn-sm btn-icon rounded-pill btn-text-google-plus">
+                        <i class="tf-icons ti ti-brand-google-filled"></i>
+                    </a>
                 </div>
             </div>
         </div>
+        <!-- /Login -->
     </div>
 </div>
-@endsection
+
+<!-- / Content -->
+
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+
+<script src="public/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="public/assets/vendor/libs/popper/popper.js"></script>
+<script src="public/assets/vendor/js/bootstrap.js"></script>
+<script src="public/assets/vendor/libs/node-waves/node-waves.js"></script>
+<script src="public/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="public/assets/vendor/libs/hammer/hammer.js"></script>
+<script src="public/assets/vendor/libs/i18n/i18n.js"></script>
+<script src="public/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+<script src="public/assets/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="public/assets/vendor/libs/@form-validation/popular.js"></script>
+<script src="public/assets/vendor/libs/@form-validation/bootstrap5.js"></script>
+<script src="public/assets/vendor/libs/@form-validation/auto-focus.js"></script>
+
+<!-- Main JS -->
+<script src="public/assets/js/main.js"></script>
+
+<!-- Page JS -->
+<script src="public/assets/js/pages-auth.js"></script>
+</body>
+</html>
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -63,6 +63,25 @@
 
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="public/assets/js/config.js"></script>
+
+    <style>
+
+        .app-brand-logo.demo {
+            -ms-flex-align: center;
+            align-items: center;
+            -ms-flex-pack: center;
+            justify-content: center;
+            display: -ms-flexbox;
+            display: flex;
+            width: 180px;
+            height: 68px;
+        }
+        .dark-style .menu .app-brand.demo {
+            height: 120px;
+        }
+    </style>
+
+
 </head>
 
 <body>
@@ -72,35 +91,12 @@
         <!-- Menu -->
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-            <div class="app-brand demo">
-                <a href="index.html" class="app-brand-link">
+            <div class="app-brand demo ">
+                    <a href="index.html" class="app-brand-link">
               <span class="app-brand-logo demo">
-                <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                      fill="#7367F0" />
-                  <path
-                      opacity="0.06"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                      fill="#161616" />
-                  <path
-                      opacity="0.06"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                      fill="#161616" />
-                  <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                      fill="#7367F0" />
-                </svg>
+                               <img src="{{url('public/assets/Logo.png')}}" alt="Taxi-Link Logo" width="100%" height="100px"/>
               </span>
-                    <span class="app-brand-text demo menu-text fw-bold">Vuexy</span>
+{{--                    <span class="app-brand-text demo menu-text fw-bold">Vuexy</span>--}}
                 </a>
 
                 <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
